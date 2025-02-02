ed651dc3872e6eec8b98fd3991159c59ea976446
justtdream <alina.dauzhanova@gmail.com>
2025-02-03 02:31:14 +0500

    add file

---
+++ fun1_14.py
@@ -0,0 +1,9 @@
+from fun1_13 import game
+
+print("do you wanna play a game? yes or no")
+answ = input()
+if answ == "yes":
+    game()
+else:
+    print(" you have to play anyways HEHEHEHEHE ")
+    game()
