5aadd9aa988577112b3f81731bb80b3ef2df1701
justtdream <alina.dauzhanova@gmail.com>
2025-02-02 13:53:20 +0500

    modify file

--- fun1_1.py
+++ fun1_1.py
@@ -1,13 +1,20 @@
-def sort_prime(n):
+def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
+    for i in range (2, int(n**0.5) +1):
+        if n%i == 0:
+            return False 
     return True
 
-def filter_prime(numbers):
-    return [n for n in numbers if sort_prime(n)]
+def filter_prime(inputList):
+    resList = []
+    for n in inputList:
+        if is_prime(int(n)):
+            resList.append(n)
+    return resList
 
-print(filter_prime(map(int, input().split())))
+listIn = list(input("Enter numbers: ").split())
+print("Prime numbers: ")
+for n in filter_prime(listIn):
+    print(n)
 
