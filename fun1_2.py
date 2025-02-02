ffeeda1c0b9c73b3012b3eefec467a049b257028
justtdream <alina.dauzhanova@gmail.com>
2025-02-03 00:58:50 +0500

    edit file

--- fun1_2.py
+++ fun1_2.py
@@ -1,9 +1,6 @@
-def has_33(nums):
-    for i in range (len(nums)-1):
-        if nums[i] == 3 and nums[i+1] == 3:
-            return True 
-    return False
+def temp (num):
+    return ((5/9)*(num-32))
 
-print (has_33([1, 3, 3]))
-print (has_33([1, 3, 1, 3]))
-print (has_33([3, 1, 3]))
+num = (float(input()))
+answer = temp(num)
+print(answer)
