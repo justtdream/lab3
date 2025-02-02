4248d04fc5acf1f4e3fdb4c655e81335a07a6524
justtdream <alina.dauzhanova@gmail.com>
2025-02-02 11:49:29 +0500

    add file

---
+++ fun1_1.py
@@ -0,0 +1,14 @@
+def filter (n):
+    if n < 2:
+        return False
+    for i in range (2, int(n**0.5) +1):
+        if n%i == 0:
+            return False 
+    return True
+n = num
+answer1 = filter (n)
+print (answer1)
+
+num = input()
+list = num.split()
+print (list)
